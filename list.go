780a99f14ee252610183019a17c5c7a4e305df42
hsldymq <hsldymq@qq.com>
2024-01-12 18:04:51 +0800

    update

--- list.go
+++ list.go
@@ -10,6 +10,11 @@ type ICollection[T any] interface {
 	Remove(T) bool
 }
 
+type ItemInfo1[T any] struct {
+	Index int
+	Item  T
+}
+
 type IList[T any] interface {
 	ICollection[T]
 
@@ -19,8 +24,32 @@ type IList[T any] interface {
 	RemoveAt(index int) error
 	Clear()
 
+	Filter(filter func(each ItemInfo1[T]) bool) IList[T]
+	SelfFilter(filter func(each ItemInfo1[T]) bool)
 	Sort(comparer func(a T, b T) bool) IList[T]
 	SelfSort(comparer func(a T, b T) bool)
 	Shuffle() IList[T]
-	SelfShuffle() IList[T]
+	SelfShuffle()
+}
+
+type List[T any] struct {
+	coll []T
+}
+
+func NewList[T any]() *List[T] {
+	return &List[T]{}
+}
+
+func (l *List[T]) Add(item T) {
+	l.coll = append(l.coll, item)
+}
+
+func (l *List[T]) Filter(filterFunc func(item ItemInfo1[T]) bool) IList[T] {
+	newList := NewList[T]()
+	for idx, item := range newList.coll {
+		if filterFunc(ItemInfo1[T]{Index: idx, Item: item}) {
+			newList.Add(item)
+		}
+	}
+	return newList
 }
