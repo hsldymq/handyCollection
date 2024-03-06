0bbddde7b8adee4883cdeee34135398aa2c268a8
hsldymq <hsldymq@qq.com>
2024-03-06 16:07:14 +0800

    update: List

--- list.go
+++ list.go
@@ -5,40 +5,51 @@ package handyCollection
 import (
 	"github.com/hsldymq/goiter"
 	"iter"
+	"reflect"
 	"slices"
 )
 
 type List[T any] struct {
-	coll []T
+	elems      []T
+	comparable bool
 }
 
 func NewList[T any]() *List[T] {
-	return &List[T]{}
+	return &List[T]{
+		comparable: reflect.ValueOf(zeroVal[T]()).Comparable(),
+	}
 }
 
 func (l *List[T]) Get(idx int) (T, bool) {
-	if idx >= len(l.coll) || idx < 0 {
+	if idx >= len(l.elems) || idx < 0 {
 		return zeroVal[T](), false
 	}
-	return l.coll[idx], true
+	return l.elems[idx], true
 }
 
-func (l *List[T]) IndexOf(item T) int {
-	for idx, elem := range l.coll {
+func (l *List[T]) Index(item T) (result int) {
+	result = -1
+	if !l.comparable {
+		return
+	}
+
+	defer func() { recover() }()
+	for idx, elem := range l.elems {
 		if shallowEqual(elem, item) {
-			return idx
+			result = idx
+			break
 		}
 	}
 
-	return -1
+	return
 }
 
 func (l *List[T]) Contains(v T) bool {
-	return l.IndexOf(v) != -1
+	return l.Index(v) >= 0
 }
 
 func (l *List[T]) Add(items ...T) {
-	l.coll = slices.Concat(l.coll, items)
+	l.elems = slices.Concat(l.elems, items)
 }
 
 func (l *List[T]) Concat(tl ...*List[T]) {
@@ -47,11 +58,11 @@ func (l *List[T]) Concat(tl ...*List[T]) {
 	}
 
 	s := make([][]T, 0, len(tl)+1)
-	s = append(s, l.coll)
+	s = append(s, l.elems)
 	for _, each := range tl {
-		s = append(s, each.coll)
+		s = append(s, each.elems)
 	}
-	l.coll = slices.Concat(s...)
+	l.elems = slices.Concat(s...)
 }
 
 func (l *List[T]) ConcatSlices(sl ...[]T) {
@@ -60,30 +71,30 @@ func (l *List[T]) ConcatSlices(sl ...[]T) {
 	}
 
 	s := make([][]T, 0, len(sl)+1)
-	s = append(s, l.coll)
+	s = append(s, l.elems)
 	s = append(s, sl...)
-	l.coll = slices.Concat(s...)
+	l.elems = slices.Concat(s...)
 }
 
 func (l *List[T]) Clone() *List[T] {
 	clonedList := NewList[T]()
-	clonedList.coll = make([]T, 0, l.Count())
-	copy(clonedList.coll, l.coll)
+	clonedList.elems = make([]T, 0, l.Count())
+	copy(clonedList.elems, l.elems)
 	return clonedList
 }
 
 func (l *List[T]) Clear() {
-	l.coll = l.coll[:0]
+	l.elems = l.elems[:0]
 }
 
 func (l *List[T]) Count() int {
-	return len(l.coll)
+	return len(l.elems)
 }
 
 func (l *List[T]) Iter() iter.Seq[T] {
-	return goiter.SliceElem(l.coll)
+	return goiter.SliceElem(l.elems)
 }
 
 func (l *List[T]) Iter2() iter.Seq2[int, T] {
-	return goiter.Slice(l.coll)
+	return goiter.Slice(l.elems)
 }
