7f0093c14e4707a572720353556d08feac1b0109
hsldymq <hsldymq@qq.com>
2024-03-18 10:46:34 +0800

    add: Transform functions

---
+++ transformation.go
@@ -0,0 +1,31 @@
+package handy
+
+import (
+	"github.com/hsldymq/goiter"
+	"iter"
+)
+
+func Transform[In any, Out any](e Enumerable[In], transformer func(In) Out) Enumerable[Out] {
+	return newEnumerator(goiter.T1(e.Iter(), transformer))
+}
+
+func TransformExpand[In any, Out any](e Enumerable[In], transformer func(In) Enumerable[Out]) Enumerable[Out] {
+	seq := func(yield func(Out) bool) {
+		next, stop := iter.Pull(e.Iter())
+		defer stop()
+		for {
+			v, ok := next()
+			if !ok {
+				return
+			}
+
+			transformed := transformer(v)
+			for each := range transformed.Iter() {
+				if !yield(each) {
+					return
+				}
+			}
+		}
+	}
+	return newEnumerator(seq)
+}
