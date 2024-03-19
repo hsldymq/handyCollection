3e43a672673cbc5eb85db55b30c8d07f40bccb58
hsldymq <hsldymq@qq.com>
2024-03-19 18:48:48 +0800

    update: seq functions

--- funcs_sequence.go
+++ funcs_sequence.go
@@ -5,7 +5,7 @@ import (
 	"iter"
 )
 
-func sequenceEqual[T any](e1, e2 Enumerable[T]) bool {
+func sequenceEqual[T any](e1, e2 Iterable[T]) bool {
 	typeComparable := isTypeComparable[T]()
 	_, comparableImpl := any(zeroVal[T]()).(Comparable)
 	if comparableImpl {
@@ -16,7 +16,7 @@ func sequenceEqual[T any](e1, e2 Enumerable[T]) bool {
 	return false
 }
 
-func sequenceEqualBy[T any](e1, e2 Enumerable[T], keySelector func(T) any) bool {
+func sequenceEqualBy[T any](e1, e2 Iterable[T], keySelector func(T) any) bool {
 	next1, stop1 := iter.Pull(e1.Iter())
 	defer stop1()
 	next2, stop2 := iter.Pull(e2.Iter())
@@ -36,7 +36,7 @@ func sequenceEqualBy[T any](e1, e2 Enumerable[T], keySelector func(T) any) bool 
 	}
 }
 
-func concat[T any](e Enumerable[T], iterables ...Iterable[T]) Enumerable[T] {
+func concat[T any](e Iterable[T], iterables ...Iterable[T]) Enumerable[T] {
 	seqs := []iter.Seq[T]{e.Iter()}
 	for _, each := range iterables {
 		seqs = append(seqs, each.Iter())
