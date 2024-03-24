36950ecc09e5a50235ea8f11e46b33f1968a38bb
hsldymq <hsldymq@qq.com>
2024-03-24 14:33:46 +0800

    add: Dict

--- dict.go
+++ dict.go
@@ -8,3 +8,20 @@ type KVPair[K comparable, V any] struct {
 func (p KVPair[K, V]) ComparableKey() any {
 	return p.Key
 }
+
+func NewDict[K comparable, V any]() *Dict[K, V] {
+	return &Dict[K, V]{}
+}
+
+type Dict[K comparable, V any] struct {
+	// TODO
+}
+
+func (d *Dict[K, V]) Set(key K, val V) {
+	// TODO
+}
+
+func (d *Dict[K, V]) SetOnce(key K, val V) bool {
+	// TODO
+	return true
+}
