bc2ce3ae4878c131b35bc284da766f140ead8ec3
hsldymq <hsldymq@qq.com>
2024-03-28 14:08:51 +0800

    refactor

---
+++ funcs_combining.go
@@ -0,0 +1,15 @@
+package handy
+
+import (
+	"github.com/hsldymq/goiter"
+	"iter"
+)
+
+func concat[T any](e Iterable[T], iterables ...Iterable[T]) Enumerable[T] {
+	seqs := []iter.Seq[T]{}
+	for _, each := range iterables {
+		seqs = append(seqs, each.Iter())
+	}
+
+	return newEnumerator(goiter.Concat(e.Iter(), seqs...))
+}
--- funcs_sequence.go
+++ funcs_sequence.go
@@ -1,18 +1,8 @@
 package handy
 
 import (
-	"github.com/hsldymq/goiter"
 	"iter"
 )
-
-func concat[T any](e Iterable[T], iterables ...Iterable[T]) Enumerable[T] {
-	seqs := []iter.Seq[T]{}
-	for _, each := range iterables {
-		seqs = append(seqs, each.Iter())
-	}
-
-	return newEnumerator(goiter.Concat(e.Iter(), seqs...))
-}
 
 func sequenceEqual[T any](e1, e2 Iterable[T]) bool {
 	typeComparable := isTypeComparable[T]()
