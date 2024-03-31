dfbd5a0e56fa4fb5909fb46883a0b22081043666
hsldymq <hsldymq@qq.com>
2024-03-31 13:22:14 +0800

    fix: goiter Fold has renmaed

--- funcs_aggregation.go
+++ funcs_aggregation.go
@@ -1,13 +1,13 @@
 package handy
 
 import (
-	"github.com/hsldymq/goiter"
+    "github.com/hsldymq/goiter"
 )
 
 func Reduce[T, R any](e Iterable[T], init R, reducer func(R, T) R) R {
-	return goiter.Fold(e.Iter(), init, reducer)
+    return goiter.Reduce(e.Iter(), init, reducer)
 }
 
 func Scan[T, R any](e Iterable[T], init R, folder func(R, T) R) Enumerable[R] {
-	return newEnumerator(goiter.Scan(e.Iter(), init, folder))
+    return newEnumerator(goiter.Scan(e.Iter(), init, folder))
 }
