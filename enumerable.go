0866dfbff5910ffee5204b0fbe337ce8273673f4
hsldymq <hsldymq@qq.com>
2024-04-01 17:59:19 +0800

    refactor

--- enumerable.go
+++ enumerable.go
@@ -3,33 +3,33 @@ package handy
 import "iter"
 
 type Iterable[T any] interface {
-	Iter() iter.Seq[T]
+    Iter() iter.Seq[T]
 }
 
 type Enumerable[T any] interface {
-	Iterable[T]
-	Count() int
-	Any(func(T) bool) bool
-	All(func(T) bool) bool
-	Filter(func(T) bool) Enumerable[T]
-	Distinct() Enumerable[T]
-	DistinctBy(func(T) any) Enumerable[T]
-	Take(int) Enumerable[T]
-	TakeLast(int) Enumerable[T]
-	Skip(int) Enumerable[T]
-	SkipLast(int) Enumerable[T]
-	Union(Enumerable[T]) Enumerable[T]
-	UnionBy(Enumerable[T], func(T) any) Enumerable[T]
-	Intersect(Enumerable[T]) Enumerable[T]
-	IntersectBy(Enumerable[T], func(T) any) Enumerable[T]
-	Expect(Enumerable[T]) Enumerable[T]
-	ExpectBy(Enumerable[T], func(T) any) Enumerable[T]
-	SequenceEqual(Enumerable[T]) bool
-	SequenceEqualBy(Enumerable[T], func(T) any) bool
-	Concat(...Iterable[T]) Enumerable[T]
-	OrderBy(func(T, T) int) Enumerable[T]
+    Iterable[T]
+    Count() int
+    Any(func(T) bool) bool
+    All(func(T) bool) bool
+    Filter(func(T) bool) Enumerable[T]
+    Take(int) Enumerable[T]
+    TakeLast(int) Enumerable[T]
+    Skip(int) Enumerable[T]
+    SkipLast(int) Enumerable[T]
+    Distinct() Enumerable[T]
+    DistinctBy(func(T) any) Enumerable[T]
+    Union(Enumerable[T]) Enumerable[T]
+    UnionBy(Enumerable[T], func(T) any) Enumerable[T]
+    Intersect(Enumerable[T]) Enumerable[T]
+    IntersectBy(Enumerable[T], func(T) any) Enumerable[T]
+    Expect(Enumerable[T]) Enumerable[T]
+    ExpectBy(Enumerable[T], func(T) any) Enumerable[T]
+    SequenceEqual(Enumerable[T]) bool
+    SequenceEqualBy(Enumerable[T], func(T) any) bool
+    Concat(...Iterable[T]) Enumerable[T]
+    OrderBy(func(T, T) int) Enumerable[T]
 }
 
 type Comparable interface {
-	ComparingKey() any
+    ComparingKey() any
 }
--- enumerator.go
+++ enumerator.go
@@ -1,110 +1,110 @@
 package handy
 
 import (
-	"github.com/hsldymq/goiter"
-	"iter"
+    "github.com/hsldymq/goiter"
+    "iter"
 )
 
 func newEnumerator[TIter goiter.SeqX[T], T any](it TIter) *Enumerator[T] {
-	return &Enumerator[T]{
-		iterator: goiter.Iterator[T](it),
-	}
+    return &Enumerator[T]{
+        iterator: goiter.Iterator[T](it),
+    }
 }
 
 type Enumerator[T any] struct {
-	iterator goiter.Iterator[T]
+    iterator goiter.Iterator[T]
 }
 
 func (e *Enumerator[T]) Iter() iter.Seq[T] {
-	return e.iterator.Seq()
+    return e.iterator.Seq()
 }
 
 func (e *Enumerator[T]) Count() int {
-	return e.iterator.Count()
+    return e.iterator.Count()
 }
 
 func (e *Enumerator[T]) Any(predicate func(T) bool) bool {
-	for each := range e.iterator {
-		if predicate(each) {
-			return true
-		}
-	}
-	return false
+    for each := range e.iterator {
+        if predicate(each) {
+            return true
+        }
+    }
+    return false
 }
 
 func (e *Enumerator[T]) All(predicate func(T) bool) bool {
-	for each := range e.iterator {
-		if !predicate(each) {
-			return false
-		}
-	}
-	return true
+    for each := range e.iterator {
+        if !predicate(each) {
+            return false
+        }
+    }
+    return true
 }
 
 func (e *Enumerator[T]) Filter(predicate func(T) bool) Enumerable[T] {
-	return filter(e, predicate)
-}
-
-func (e *Enumerator[T]) Distinct() Enumerable[T] {
-	return distinct[T](e)
-}
-
-func (e *Enumerator[T]) DistinctBy(keySelector func(T) any) Enumerable[T] {
-	return distinctBy(e, keySelector)
+    return filter(e, predicate)
 }
 
 func (e *Enumerator[T]) Take(n int) Enumerable[T] {
-	return newEnumerator(goiter.Take(e.Iter(), n))
+    return newEnumerator(goiter.Take(e.Iter(), n))
 }
 
 func (e *Enumerator[T]) TakeLast(n int) Enumerable[T] {
-	return newEnumerator(goiter.TakeLast(e.Iter(), n))
+    return newEnumerator(goiter.TakeLast(e.Iter(), n))
 }
 
 func (e *Enumerator[T]) Skip(n int) Enumerable[T] {
-	return newEnumerator(goiter.Skip(e.Iter(), n))
+    return newEnumerator(goiter.Skip(e.Iter(), n))
 }
 
 func (e *Enumerator[T]) SkipLast(n int) Enumerable[T] {
-	return newEnumerator(goiter.SkipLast(e.Iter(), n))
+    return newEnumerator(goiter.SkipLast(e.Iter(), n))
+}
+
+func (e *Enumerator[T]) Distinct() Enumerable[T] {
+    return distinct[T](e)
+}
+
+func (e *Enumerator[T]) DistinctBy(keySelector func(T) any) Enumerable[T] {
+    return distinctBy(e, keySelector)
 }
 
 func (e *Enumerator[T]) Union(target Enumerable[T]) Enumerable[T] {
-	return union(e, target)
+    return union(e, target)
 }
 
 func (e *Enumerator[T]) UnionBy(target Enumerable[T], keySelector func(T) any) Enumerable[T] {
-	return unionBy(e, target, keySelector)
+    return unionBy(e, target, keySelector)
 }
 
 func (e *Enumerator[T]) Intersect(target Enumerable[T]) Enumerable[T] {
-	return intersect(e, target)
+    return intersect(e, target)
 }
 
 func (e *Enumerator[T]) IntersectBy(target Enumerable[T], keySelector func(T) any) Enumerable[T] {
-	return intersectBy(e, target, keySelector)
+    return intersectBy(e, target, keySelector)
 }
 
 func (e *Enumerator[T]) Expect(target Enumerable[T]) Enumerable[T] {
-	return except(e, target)
+    return except(e, target)
 }
 
 func (e *Enumerator[T]) ExpectBy(target Enumerable[T], keySelector func(T) any) Enumerable[T] {
-	return exceptBy(e, target, keySelector)
+    return exceptBy(e, target, keySelector)
 }
 
 func (e *Enumerator[T]) SequenceEqual(target Enumerable[T]) bool {
-	return sequenceEqual[T](e, target)
+    return sequenceEqual[T](e, target)
 }
 
 func (e *Enumerator[T]) SequenceEqualBy(target Enumerable[T], keySelector func(T) any) bool {
-	return sequenceEqualBy(e, target, keySelector)
+    return sequenceEqualBy(e, target, keySelector)
 }
 
 func (e *Enumerator[T]) Concat(iterables ...Iterable[T]) Enumerable[T] {
-	return concat(e, iterables...)
+    return concat(e, iterables...)
 }
 
 func (e *Enumerator[T]) OrderBy(cmp func(a, b T) int) Enumerable[T] {
-	return orderBy(e, cmp)
+    return orderBy(e, cmp)
 }
